2f475a8700245386ba07138b45d89065481b40da
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2024-04-05 22:25:23 +0200

    wip

--- src/main/java/org/esupportail/cas/adaptors/esupotp/EsupOtpCredential.java
+++ src/main/java/org/esupportail/cas/adaptors/esupotp/EsupOtpCredential.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apereo.cas.authentication.Credential;
+import org.apereo.cas.authentication.credential.OneTimeTokenCredential;
 
 /**
  * This is {@link EsupOtpCredential}.
@@ -13,70 +13,8 @@ import org.apereo.cas.authentication.Credential;
  * @author Alex Bouskine
  * @since 5.0.0
  */
-public class EsupOtpCredential implements Credential, Serializable {
+public class EsupOtpCredential extends OneTimeTokenCredential {
 	
     private static final long serialVersionUID = -7570600701132111037L;
 
-    private String token;
-
-	/**
-     * Instantiates a new Esup otp token credential.
-     */
-    public EsupOtpCredential() {
-    }
-
-    /**
-     * Instantiates a new Esup otp credential.
-     *
-     * @param token the token
-     */
-    public EsupOtpCredential(final String token) {
-        this.token = token;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("token", this.token)
-                .toString();
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (!(obj instanceof EsupOtpCredential)) {
-            return false;
-        }
-        if (obj == this) {
-            return true;
-        }
-        final EsupOtpCredential other = (EsupOtpCredential) obj;
-        final EqualsBuilder builder = new EqualsBuilder();
-        builder.append(this.token, other.token);
-        return builder.isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        final HashCodeBuilder builder = new HashCodeBuilder(97, 31);
-        builder.append(this.token);
-        return builder.toHashCode();
-    }
-
-    @Override
-    public String getId() {
-        return this.token;
-    }
-
-
-    public String getToken() {
-        return this.token;
-    }
-
-    public void setToken(final String token) {
-        this.token = token;
-    }
-
-    public boolean isValid() {
-        return this.token != null;
-    }
 }
